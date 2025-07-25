47965db2158873c3f58ae088c227bb7ce45bbc4b
siddeshwaran <siddeswaran@ledgers.cloud>
2025-07-25 14:53:34 +0530

    my try to update the ogic

--- day1/find_duplicate.py
+++ day1/find_duplicate.py
@@ -13,19 +13,39 @@
 #     #     seen.add(num)
 #     # return None
 
+# def find_duplicates(nums): #creating function to find dupliacte nums are mentioned as list of numbers as input
+
+#     #count the frequency of each number in the list
+#     frequency = {}
+#     #create a list to store the duplicate numbers
+#     result = []
+
+#     for num in nums:
+#         frequency[num] = frequency.get(num, 0) + 1
+
+#     for key in frequency:
+#         if frequency[key] > 1:
+#             result.append(key)
+
+#     return result
+
+# print(find_duplicates([1, 2, 3, 2, 3, 4, 5, 1]))
+
 def find_duplicates(nums):
-    frequency = {}
-    result = []
+    frequency={}
+    result=[]
 
     for num in nums:
-        frequency[num] = frequency.get(num, 0) + 1
-
+        frequency[num] =frequency.get(num,0) +1
+    
     for key in frequency:
-        if frequency[key] > 1:
-            result.append(key)
+
+     if frequency[key] > 1:
+      result.append(key)
 
     return result
 
-print(find_duplicates([1, 2, 3, 2, 3, 4, 5, 1]))
+
+print(find_duplicates([1,2,1,3,5,4,1,5,3]))
 
     
